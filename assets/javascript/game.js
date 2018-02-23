17ebe617b25f1dbdc84cdb58166a3bcdbf837450
Ghusoon <ga.ghusoon@gmail.com>
2018-02-23 11:27:35 -0500

    Hangman game with image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,59 +3,66 @@
 var winTimes =1;
 var wins = 0;
 var loses = 10;
-var words =[ "blue","green","red","yellow"];
+var words =[ "panda", "dolphin","koala"];
 var wrongLetter=[""];
 var underscore=[];
-var image =[];
+
+function gameStar(){
+
 var randWord = words[Math.floor(Math.random() * words.length)];{
     console.log(randWord);
-  
 }
 
+//image show after winning.
+function image(){
+    var image = document.getElementById("image").innerHTML;
+    if (randWord===words[0]){
 
+         image.src = "assets/images/panda.jpg";
+        console.log(image);
+    }
+    if (randWord===words[1]){
 
+        image.src = "assets/images/dolphin.jpg";
+       console.log(image);
+   }
+    if (randWord===words[2]){
+
+    image.src = "assets/images/koala.png";
+   console.log(image);
+}
+}
+
+// reset game after winnig.
 function resetGame() {
-    document.getElementById("underscore").innerHTML="";
+  
     loses = 10;
     wrongLetter = [];
-    randWord = words[Math.floor(Math.random() * words.length)];
+    randWord; 
     console.log(randWord);
     
 for ( var i=0 ; i< randWord.length; i++){
     underscore[i] =" __ ";
        console.log(underscore); 
-      
+       document.getElementById("underscore").innerHTML= ("")+ underscore.join("");
     }
-
+    
 }
 
-
+resetGame();
 
 //the underscore.
 
 
-for ( var i=0 ; i< randWord.length; i++){
-underscore[i] =" __ ";
-   console.log(underscore); 
-  
-}
-
-        document.getElementById("underscore").innerHTML= ("")+ underscore.join("");
        document.getElementById("wins").innerHTML= wins;
        document.getElementById("loses").innerHTML= loses;  
 
-//function newword(){
-    // setTimeout( document.getElementById("underscore")("")+ underscore.join(""),2000);
-   
-
-    
-// newword();  
 
 //game start
 document.onkeyup = function(event) {
    var  userGuess = event.key;
    console.log(userGuess);
-//    newword();
+
 
   if( randWord.indexOf(userGuess)>-1){
 
@@ -65,18 +72,20 @@ document.onkeyup = function(event) {
     
         underscore[j] = userGuess;
        winTimes++;
-        
+       console.log(userGuess); 
     }
-}
-
- if (winTimes==(randWord.length) + 1){
+    }
+   
+ if (winTimes===((randWord.length) +1)){
        
     wins++;
-    resetGame()
+    image();
+    resetGame();
 //    newword();  
     console.log(wins);
-   
- }
+
+} 
+    
  //if ( image[0]===words[0]){
     //image.sre("")
  //}
@@ -93,14 +102,14 @@ document.onkeyup = function(event) {
        
     }   
 
-    resetGame();
+   // resetGame();
     document.getElementById("underscore").innerHTML= ("")+ underscore.join("");
     document.getElementById("wins").innerHTML= wins;
     document.getElementById("loses").innerHTML= loses;
     document.getElementById("letters").innerHTML=  wrongLetter.join("");
         }
+    }
     
-    
-
+    gameStar();
 
 
